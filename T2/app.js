1b1fb6d974be1f88ff81c4464ac7e06003194a3f
ankitchawla3123 <ankitchawla3123@gmail.com>
2025-07-19 12:00:33 +0530

    route check

--- T2/app.js
+++ T2/app.js
@@ -14,6 +14,27 @@ app.get("/hey", (req, res) => {
   res.send("TO1");
 });
 
+app.get("/messages", (req, res) =>
+  res.send("This is where you can see any messages.")
+);
+
+// paths
+// both message and messages will work in below example
+app.get("/message{s}", (req, res) =>
+  res.send("This is where you can see any messages.")
+);
+
+app.get("/hey{/yo}/how", (req, res) => {
+  res.send(
+    "Path check for hey{/yo}/how will work for both hey/how and also hey/yo/how "
+  );
+});
+
+// must be at last as order matters
+app.get("/{*splat}", (req, res) => {
+  res.send("error 404 ");
+});
+
 app.listen(PORT, () => {
   console.log("express at this port", process.env.PORT);
 });
