e707833d5df8fcd05074ad8a0900af54c63677b3
ankitchawla3123 <ankitchawla3123@gmail.com>
2025-07-19 12:35:17 +0530

    few more routing technique learned

--- T2/app.js
+++ T2/app.js
@@ -30,6 +30,35 @@ app.get("/hey{/yo}/how", (req, res) => {
   );
 });
 
+//params
+app.get("/:username/messages", (req, res) => {
+  res.send(`username ${req.params.username}`); // in object format
+});
+
+app.get("/:username/messages/:messageid", (req, res) => {
+  console.log(req.params);
+  res.send("will be in object format");
+});
+
+// query params ? denotes the start of it
+
+// not actual path but more like an argument
+// these are seperated by the & symbol and they act as key value pairs
+
+// http://localhost:5173/dasda/queryparam?key=hello&anotherkey=yo&sort=date&direction=ascending&sort=anothersortwillbeinanarray
+app.get("/:username/queryparam", (req, res) => {
+  console.log(req.query);
+  res.send("check the console to see if any query params ");
+});
+
+app.get("{/:username}/uniquepathcheck", (req, res) => {
+  res.send(
+    `unique path exist with username ${
+      req.params.username !== undefined ? req.params.username : "no usrename"
+    }`
+  );
+});
+
 // must be at last as order matters
 app.get("/{*splat}", (req, res) => {
   res.send("error 404 ");
