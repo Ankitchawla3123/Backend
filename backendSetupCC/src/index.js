fa9297fdec1d46b4b3d1a7fecf49d5c87e49c847
ankitchawla3123 <ankitchawla3123@gmail.com>
2025-07-22 03:12:20 +0530

    express setup

--- backendSetupCC/src/index.js
+++ backendSetupCC/src/index.js
@@ -1,13 +1,27 @@
 // require('dotenv').config({path:"./env"})
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
   path: "./env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.on("error ")
+    app.listen(process.env.PORT || 8000, () => {
+      console.log("server is running at port", process.env.PORT);
+    });
+  })
+  .catch((err) => {
+    console.log("DB CONNECTION FAILED", err);
+  });
 
+
+
+
+  
 /*
 import mongoose from "mongoose";
 
