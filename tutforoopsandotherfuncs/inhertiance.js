a33325895d90fc1ea9e59b68a0326f26053e5747
ankitchawla3123 <ankitchawla3123@gmail.com>
2025-07-22 17:55:52 +0530

    few more tweaks :

--- tutforoopsandotherfuncs/inhertiance.js
+++ tutforoopsandotherfuncs/inhertiance.js
@@ -11,7 +11,7 @@ class Person {
 
 class Student extends Person {
   constructor(name, age, grade, section) {
-    super(name, age);
+    super(name, age); // super to be on top of the constructor class
     this.grade = grade;
     this.section = section;
   }
@@ -24,3 +24,12 @@ class Student extends Person {
 const s1 = new Student("SPY", 22, 11, "B");
 s1.studentinfo();
 s1.introduce();
+const p1 = new Person("SPEED", 22);
+p1.introduce();
+
+p1.studentinfo(); // error , comment this for futher use
+
+// hack can be used we can set prototype properties in p1 from student
+Object.setPrototypeOf(p1, Student.prototype);
+
+p1.studentinfo();
